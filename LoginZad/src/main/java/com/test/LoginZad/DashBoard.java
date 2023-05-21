b1a589e93e95e571c8f29fde732ffd6515bf6484
zad6 <the_powah@hotmail.com>
2023-05-21 13:21:58 +0200

    Subo todo el proyecto

---
+++ LoginZad/src/main/java/com/test/LoginZad/DashBoard.java
@@ -0,0 +1,38 @@
+package com.test.LoginZad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class DashBoard extends AppCompatActivity {
+
+
+    private Button btnSalir;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board);
+
+
+        btnSalir = findViewById(R.id.btnSalir);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnSalir.setOnClickListener(view -> {
+            mAuth.signOut();
+            startActivity(new Intent(this, LoginActivity.class));
+
+        });
+
+    }//End onCreate
+
+
+
+}
